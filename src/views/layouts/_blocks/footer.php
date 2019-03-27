f9bb5a34108bf86c068c117bfadec1722a18d0c7
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-27 16:48:15 +0300

    fix

--- src/views/layouts/_blocks/footer.php
+++ src/views/layouts/_blocks/footer.php
@@ -6,7 +6,7 @@
 ?>
 
 <div class="page-footer">
-	Copyright &copy; 2014-<?=date("Y")?>
+	Copyright &copy; 2014-<?=date("Y")?>&nbsp;
 	<a href="http://leadcode.ru" target="_blank">LeadCode</a>.
 	All rights reserved.
 </div>
