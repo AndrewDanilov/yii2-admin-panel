695c071c5f54803259c4e78159ac93b28c356e08
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-28 13:08:48 +0300

    fix

--- src/views/site/login.php
+++ src/views/site/login.php
@@ -21,7 +21,7 @@ $fieldOptions = [
 	<!-- Outer Row -->
 	<div class="row justify-content-center">
 
-		<div class="col-xl-5 col-lg-6 col-md-9">
+		<div class="col-xs-8 col-sm-6 col-md-4">
 
 			<div class="card o-hidden border-0 shadow-lg my-5">
 				<div class="card-body p-0">
