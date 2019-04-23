54601c74e216f5fdde5f7ced95cc179c28321af9
Andrey Danilov <andrewdanilov@gmail.com>
2019-04-23 12:43:33 +0300

    error page and layout

--- src/views/layouts/error.php
+++ src/views/layouts/error.php
@@ -23,7 +23,7 @@ AdminPanelAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="page-wrapper flex-center">
+<div class="page-wrapper justify-content-center align-content-center">
 	<?= $content ?>
 </div>
 
--- src/views/site/error.php
+++ src/views/site/error.php
@@ -3,21 +3,13 @@
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
-/* @var $name string */
-/* @var $message string */
-/* @var $exception Exception */
 
-$this->title = $name;
+$this->title = 'Error 404. Page Not Found.';
 ?>
-<div class="container-fluid">
-
-	<!-- 404 Error Text -->
-	<div class="text-center">
-		<div style="font-size:100px">404</div>
-		<p class="lead">Page Not Found</p>
-		<p>It looks like you found a glitch in the matrix...</p>
-		<a href="<?= Url::to(['index']) ?>">&larr; Back to Dashboard</a>
-	</div>
-
+<div class="text-center">
+	<div class="title-404">404</div>
+	<p class="lead">Page Not Found</p>
+	<p>It looks like you found a glitch in the matrix...</p>
+	<a href="<?= Url::to(['index']) ?>">&larr; Back to Dashboard</a>
 </div>
 
