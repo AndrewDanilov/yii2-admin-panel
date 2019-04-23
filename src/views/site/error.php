f48fa58db6525f33e4a5d22f3eab2da1aae844fa
Andrey Danilov <andrewdanilov@gmail.com>
2019-04-23 12:29:22 +0300

    error page and layout

--- src/views/site/error.php
+++ src/views/site/error.php
@@ -8,16 +8,15 @@ use yii\helpers\Url;
 /* @var $exception Exception */
 
 $this->title = $name;
-
 ?>
 <div class="container-fluid">
 
 	<!-- 404 Error Text -->
 	<div class="text-center">
-		<div class="error mx-auto" data-text="404">404</div>
-		<p class="lead text-gray-800 mb-5">Page Not Found</p>
-		<p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-		<a href="<?= Url::to(['site/index']) ?>">&larr; Back to Dashboard</a>
+		<div style="font-size:100px">404</div>
+		<p class="lead">Page Not Found</p>
+		<p>It looks like you found a glitch in the matrix...</p>
+		<a href="<?= Url::to(['index']) ?>">&larr; Back to Dashboard</a>
 	</div>
 
 </div>
