ea5134e2c477e0767f3fbc241e4f1458eb1b3907
andrey <6097033725>
2022-03-17 16:34:51 +0300

    fixed migration for adding is_admin field if it was already added earlier

--- src/migrations/m201026_111558_user_table_add_is_admin_field.php
+++ src/migrations/m201026_111558_user_table_add_is_admin_field.php
@@ -12,7 +12,9 @@ class m201026_111558_user_table_add_is_admin_field extends Migration
      */
     public function safeUp()
     {
-		$this->addColumn('{{%user}}', 'is_admin', $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(0));
+	    try {
+			$this->addColumn('{{%user}}', 'is_admin', $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(0));
+	    } catch (\Exception $e) {}
 
 	    $this->upsert('{{%user}}', [
 		    'username' => 'admin',
